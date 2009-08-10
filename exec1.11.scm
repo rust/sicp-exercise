aaa84f75bdcb0f7ff26c6f8a94773af5eb588cac
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2009-08-10 10:20:15 +0900

    Update 1.11. Signed-off-by: Shin-ichiro OGAWA <rust.stnard+git@gmail.com>

--- exec1.11.scm
+++ exec1.11.scm
@@ -1,12 +1,12 @@
 ;; recursive process
 (define (f n)
-  (cond ((<= n 3) n)
+  (cond ((< n 3) n)
         (else (+ (f (- n 1)) (* 2 (f (- n 2))) (* 3 (f (- n 3)))))))
 
 ;; iterative process
 (define (f-n n)
-  (cond ((<= n 3) n)
-        (else (f-iter 3 2 1 1 (- n 3)))))
+  (cond ((< n 3) n)
+        (else (f-iter 2 1 0 1 (- n 2)))))
 
 (define (f-iter f3 f2 f1 counter max-count)
   (if (> counter max-count)
