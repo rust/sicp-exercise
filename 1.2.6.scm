1044a4c883113f9a14294f0149b8bca1f62c0c55
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2009-08-20 22:28:30 +0900

    For Section 1.2.6.
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard+git@gmail.com>

---
+++ 1.2.6.scm
@@ -0,0 +1,35 @@
+(define (square x)
+  (* x x ))
+(define (smallest-divisor n)
+  (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+        ((divide? test-divisor n ) test-divisor)
+        (else (find-divisor n (+ test-divisor 1)))))
+(define (divide? a b)
+  (= (remainder b a) 0))
+
+(smallest-divisor 127)
+
+(define (expmod base exp m)
+  (cond ((= exp 0) 1)
+        ((even? exp)
+         (remainder (square (expmod base (/ exp 2) m))
+                    m))
+        (else
+         (remainder (* base (expmod base (- exp 1) m))
+                    m))))
+
+(expmod 13 4 4)
+
+(define (fermat-test n)
+  (define (try-it a)
+    (= (expmod a n n) a))
+  (try-it (+ 1 (random (- n 1)))))
+
+(define (fast-prime? n times)
+  (cond ((= times 0) true)
+        ((fermat-test n) (fast-prime? n (- times 1)))
+        (else false)))
+
+(fast-prime? 123456789 10)
