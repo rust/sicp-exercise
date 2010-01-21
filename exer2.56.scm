202e25e1da7eb10cbb653c4e016dbc480653f2ec
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2010-01-21 19:36:33 +0900

    Add exercises
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard+git@gmail.com>

---
+++ exer2.56.scm
@@ -0,0 +1,61 @@
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
+
+(define (variable? x) (symbol? x))
+(define (same-variable? v1 v2)
+  (and (variable? v1) (variable? v2) (eq? v1 v2)))
+(define (make-sum a1 a2) (list '+ a1 a2))
+(define (make-product m1 m2) (list '* m1 m2))
+(define (sum? x)
+  (and (pair? x) (eq? (car x) '+)))
+(define (addend s) (cadr s))  ;; 和の加えられる方(1項目)
+(define (augend s) (caddr s)) ;; 和の加える方    (2項目)
+(define (product? x)
+  (and (pair? x) (eq? (car x) '*)))
+(define (multiplier p) (cadr p))    ;; 積の掛けられる方(1項目)
+(define (multiplicand p) (caddr p)) ;; 積の掛ける方    (1項目)
+
+(define (make-exponentiation b e)
+  (list '** b e))
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '**)))
+(define (base e) (cadr e))
+(define (exponent e) (caddr e))
+
+(define (deriv exp var)
+  (cond ((number? exp) 0)
+        ((variable? exp)
+         (if (same-variable? exp var) 1 0))
+        ((sum? exp)
+         (make-sum (deriv (addend exp) var)
+                   (deriv (augend exp) var)))
+        ((product? exp)
+         (make-sum
+          (make-product (multiplier exp)
+                        (deriv (multiplicand exp) var))
+          (make-product (deriv (multiplier exp) var)
+                        (multiplicand exp))))
+        ((exponentiation? exp)
+         (make-product
+          (exponent exp)
+          (make-product
+           (make-exponentiation (base exp) (- (exponent exp) 1))
+           (deriv (base exp) var))))
+        (else
+         (error "unknown expression type -- DERIV" exp))))
+
+(define e (make-exponentiation 'x 10)) ;; => x^10
+(deriv e 'x) ;; => (* 10 (* (** x 9) 1)) => 10 * x^9
