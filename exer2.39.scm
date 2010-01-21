bce1dbec06c595cef1e31ef83cec797920918a6d
Shin-ichiro OGAWA <rust.stnard+git@gmail.com>
2010-01-21 18:38:33 +0900

    Update Exer 2.39
    Signed-off-by: Shin-ichiro OGAWA <rust.stnard+git@gmail.com>

--- exer2.39.scm
+++ exer2.39.scm
@@ -1,3 +1,8 @@
+(define (accumulate op initial sequence)
+  (if (null? sequence)
+      initial
+      (op (car sequence)
+          (accumulate op initial (cdr sequence)))))
 (define (fold-left op initial sequence)
   (define (iter result rest)
     (if (null? rest)
@@ -8,9 +13,9 @@
 (define fold-right accumulate)
 
 (define (reverse sequence)
-  (fold-right (lambda (x y) (append (list x) y)) () sequence))
+  (fold-right (lambda (x y) (append y (list x))) () sequence))
 (reverse '(1 2 3 4 5 6))
 
 (define (reverse sequence)
-  (fold-left (lambda (x y) (append x (list y))) () sequence))
+  (fold-left (lambda (x y) (append (list y) x)) () sequence))
 (reverse '(1 2 3 4 5 6))
